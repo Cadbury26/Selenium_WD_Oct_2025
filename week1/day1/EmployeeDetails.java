a0a7530ac473b160428e1c8dc64265a36e9b7184
Cadbury26 <archanasivagurunathan@gmail.com>
2025-10-03 22:27:14 +0530

    Add files via upload

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,28 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+
+		String name = "Archana";
+		int age = 25;
+		double salary = 55000.53;
+		char gender = 'F';
+		boolean acceptTerms=true;
+		float rating = 4.5f;
+		long mobileNumber= 9865432555l;
+		long pincode = 641008;
+		char luckyNumber = '7';
+		
+		System.out.println("name =" +name);
+		System.out.println("age =" +age);
+		System.out.println("salary =" +salary);
+		System.out.println("gender =" +gender);
+		System.out.println("acceptTerms =" +acceptTerms);
+		System.out.println("rating =" +rating);
+		System.out.println("mobileNumber ="+mobileNumber);
+		System.out.println("pincode =" +pincode);
+		System.out.println("luckyNumber =" +luckyNumber);
+	}
+
+}
---
+++ week1/day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		Boolean isVisible= true;
+		long releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println(browserVersion);
+		System.out.println(browserName);
+		System.out.println(isVisible);
+		System.out.println(releaseYear);
+		System.out.println(browserLogo);
+		
+	}
+
+}
